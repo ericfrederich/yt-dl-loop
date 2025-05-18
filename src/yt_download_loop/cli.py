a8b8768cdc907fde1ca1b761ab06a2556bd60c45
Eric L Frederich <eric.frederich@gmail.com>
2025-05-18 10:52:39 -0400

    Make directory if it doesn't exist

--- src/yt_download_loop/cli.py
+++ src/yt_download_loop/cli.py
@@ -9,7 +9,7 @@ import yt_dlp
 @click.option(
     "-P",
     "download_path",
-    type=click.Path(file_okay=False, exists=True, path_type=Path),
+    type=click.Path(file_okay=False, path_type=Path),
     default=Path.cwd(),
     help="The paths where the files should be downloaded.  (this is just forwarded on to yt-dlp's -P option)",
 )
@@ -28,6 +28,12 @@ def cli_main(ctx: click.Context, download_path: Path, web: bool):
             if resp in ["quit", "exit", "q"]:
                 break
         ctx.exit(0)
+    if not download_path.exists():
+        if click.confirm(f"{download_path} does not exist; Create it?", default=True):
+            download_path.mkdir(parents=True)
+        else:
+            click.secho("Bailing")
+            ctx.exit(0)
     click.secho(f"Downloading to {download_path.absolute()}", fg="blue")
     click.secho("Starting download loop", fg="green", bold=True)
     while (url := input(click.style("enter url: ", fg="blue", bold=True))) not in ["quit", "exit", "q"]:
